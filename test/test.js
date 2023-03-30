299d85c4423e8fc13ab0cf361fb12f6d6f80b47d
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-03-30 15:37:01 +0200

    input veld voor naam ingevuld

--- test/test.js
+++ test/test.js
@@ -5,14 +5,15 @@ const fs = require("fs");
 const html = fs.readFileSync("./index.html");
 const { window } = new JSDOM(html);
 const { document } = window;
-
+//test formulier of het bestaat
 describe("Formulier", function () {
   it("bestaat", function () {
     const form = document.querySelector("form");
     assert(form);
   });
 });
-describe("Formulier", function () {
+//3 testen : voor de input velden om te kijken ofdat naam,email,bericht wel leeg is.
+describe("Formulier leeg", function () {
   it("input veld voor naam is leeg", function () {
     const inputVeldNaam = document.getElementById("naam");
     assert(inputVeldNaam.value === "", "input veld voor Naam leeg");
@@ -28,5 +29,13 @@ describe("Formulier", () => {
   it("input veld voor bericht is leeg", () => {
     const inputVeldBericht = document.getElementById("bericht");
     assert(inputVeldBericht.value === "", "input veld voor bericht leeg");
+  });
+});
+//3 testen : voor de input velden om te kijken ofdat naam,email,bericht ingevuld is.
+describe("Formulier ingevuld", () => {
+  it("input veld voor naam is ingevuld", () => {
+    const inputVeldNaam = document.getElementById("naam");
+    inputVeldNaam.value = "Ahmet";
+    assert.strictEqual(inputVeldNaam.value, "Ahmet", "ingevuld");
   });
 });
